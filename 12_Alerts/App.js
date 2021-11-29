5408f27ff134766c81a8f0f0e6abc1e61a8a9a15
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-29 07:59:43 +0100

    Alert Native, completing todo app

---
+++ 12_Alerts/App.js
@@ -0,0 +1,88 @@
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import  Header from './components/Header'
+import TodoItem from './components/TodoItem';
+import AddTodoItem from './components/AddTodoItem';
+
+export default function App() {
+  const [ todos, setTodos ] = useState([
+    {text: 'buy coffee', completed: false, key: '1'},
+    {text: 'create an app', completed: false, completed: false, key: '2'},
+    {text: 'paly football', completed: false, key: '3'},
+  ]);
+
+
+  const handleRemoveItem = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(person => person.key !== key);
+    });
+  }
+
+
+  const handleAddTodoItem = (newTodoItemTitle) => {
+    if(newTodoItemTitle.length > 3) {
+      setTodos((previousTodos) => {
+        return (  [
+            ...previousTodos,
+            {
+              text: newTodoItemTitle,
+              completed: false,
+              key: String(previousTodos.length + 1)
+            },
+        ])
+      });
+
+    } else {
+      Alert.alert('ERROR', 'Minimum 3 characters long', [
+        {text: "AGREE", onPress: () => console.log('Alert closed !')}
+      ]);
+    }
+  }
+
+
+  return (
+    <View style={styles.container}> 
+       < Header />
+      <View style={styles.content}>
+        <AddTodoItem  handleAddTodoItem={handleAddTodoItem}/>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem 
+                item={item}
+                handleRemoveItem={handleRemoveItem}
+              />
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // body
+    flex: 1,
+    backgroundColor: '#fff',
+    borderColor: 'yellow',
+    borderStyle: 'solid',
+    borderWidth: 2
+  },
+
+  content: {
+    // main
+    padding: 40,
+    borderColor: 'green',
+    borderStyle:'solid',
+    borderWidth: 2,
+    marginBottom: 3,
+    marginLeft: 3,
+    marginRight: 3,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
