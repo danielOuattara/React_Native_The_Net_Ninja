2c4d48d89342c7f9d7b0f870956edef9e5b9fa6a
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-29 07:58:24 +0100

    todo part1

---
+++ 09_Todo_App_Part_1/App.js
@@ -0,0 +1,54 @@
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import  Header from './components/Header'
+
+
+export default function App() {
+
+  const [ todos, setTodos ] = useState([
+    {text: 'buy coffee', key: '1'},
+    {text: 'create an app', key: '2'},
+    {text: 'paly football', key: '3'},
+  ]);
+
+  return (
+    <View style={styles.container}>
+       {/* header */ } 
+       < Header />
+      <View style={styles.content}>
+
+        {/* todo form */}
+        <View style={styles.list}>
+      
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    padding: 40,
+  },
+
+  list: {
+    marginTop: 20,
+  },
+    header: {
+      height: 80,
+      paddingTop: 38,
+      backgroundColor: 'coral'
+    },
+
+});
