d5d15b9ccfcd8f38bd58f04504a39d987dc30950
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-29 08:15:03 +0100

    Dismissing the keyBoard

---
+++ 13_Dismissing_Keyboard/App.js
@@ -0,0 +1,97 @@
+
+import React, {useState} from 'react';
+import { 
+  Alert, 
+  FlatList, 
+  Keyboard, 
+  StyleSheet, 
+  Text, 
+  TouchableWithoutFeedback, 
+  View, 
+} from 'react-native';
+import  Header from './components/Header'
+import TodoItem from './components/TodoItem';
+import AddTodoItem from './components/AddTodoItem';
+
+export default function App() {
+  const [ todos, setTodos ] = useState([
+    {text: 'buy coffee', completed: false, key: '1'},
+    {text: 'create an app', completed: false, completed: false, key: '2'},
+    {text: 'paly football', completed: false, key: '3'},
+  ]);
+
+  const handleRemoveItem = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(person => person.key !== key);
+    });
+  }
+
+  const handleAddTodoItem = (newTodoItemTitle) => {
+    if(newTodoItemTitle.length > 3) {
+      setTodos((previousTodos) => {
+        return (  [
+            ...previousTodos,
+            {
+              text: newTodoItemTitle,
+              completed: false,
+              key: String(previousTodos.length + 1)
+            },
+        ])
+      });
+
+    } else {
+      Alert.alert('ERROR', 'Minimum 3 characters long', [
+        {text: "AGREE", onPress: () => console.log('Alert closed !')}
+      ]);
+    }
+  }
+
+/* Note: Right now ToucheableWhithoutFeedBack allows to listen to touch anywhere 
+         on the phone screen except Form & Button zones */
+  return (
+    <TouchableWithoutFeedback onPress={() =>  {
+      console.log("Phone Screen Touched !");
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}> 
+        < Header />
+        <View style={styles.content}>
+          <AddTodoItem  handleAddTodoItem={handleAddTodoItem}/>
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => ( 
+                <TodoItem item={item} handleRemoveItem={handleRemoveItem}/>
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // body
+    flex: 1,
+    backgroundColor: '#fff',
+    borderColor: 'yellow',
+    borderStyle: 'solid',
+    borderWidth: 2
+  },
+
+  content: {
+    // main
+    padding: 40,
+    borderColor: 'green',
+    borderStyle:'solid',
+    borderWidth: 2,
+    marginBottom: 3,
+    marginLeft: 3,
+    marginRight: 3,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
---
+++ 13_Dismissing_Keyboard/components/TodoItem.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import { StyleSheet, Text,  View, TouchableOpacity } from 'react-native';
+
+
+export default function TodoItem({ item, handleRemoveItem }) {
+    return (
+        <TouchableOpacity onPress={() => handleRemoveItem(item.key)}>
+            <Text style={styles.todoItem}>{item.text}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    todoItem: {
+        padding: 12,
+        marginTop: 16,
+        color: '#606060',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        borderColor: 'coral',
+        borderRadius: 8,
+        fontSize: 18,
+        textTransform: 'capitalize'
+    },
+});
