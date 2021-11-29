bfc10083740ffa1a1c6a4eb20f0291ab54ca8a01
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-29 07:58:41 +0100

    todo part 2

---
+++ 10_Todo_App_Part_2/App.js
@@ -0,0 +1,65 @@
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import  Header from './components/Header'
+import TodoItem from './components/TodoItem';
+
+export default function App() {
+
+  const [ todos, setTodos ] = useState([
+    {text: 'buy coffee', completed: false, key: '1'},
+    {text: 'create an app', completed: false, completed: false, key: '2'},
+    {text: 'paly football', completed: false, key: '3'},
+  ]);
+
+  const handleRemoveItem = (key) => {
+    setTodos((prevPeople) => {
+      return prevPeople.filter(person => person.key !== key);
+    });
+  }
+
+  return (
+    <View style={styles.container}> 
+       < Header />
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem 
+                item={item}
+                handleRemoveItem={handleRemoveItem}
+              />
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // body
+    flex: 1,
+    backgroundColor: '#fff',
+    borderColor: 'yellow',
+    borderStyle: 'solid',
+    borderWidth: 2
+  },
+
+  content: {
+    // main
+    padding: 40,
+    borderColor: 'green',
+    borderStyle:'solid',
+    borderWidth: 2,
+    marginBottom: 3,
+    marginLeft: 3,
+    marginRight: 3,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
---
+++ 10_Todo_App_Part_2/components/TodoItem.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { StyleSheet, Text,  View, TouchableOpacity } from 'react-native';
+
+
+export default function TodoItem({ item, handleRemoveItem }) {
+    return (
+        <TouchableOpacity onPress={() => handleRemoveItem(item.key)}>
+            <Text style={styles.todoItem}>{item.text}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    todoItem: {
+        padding: 16,
+        marginTop: 16,
+        color: '#bbb',
+        borderWidth: 1,
+        borderStyle: 'dashed',
+        backgroundColor: 'coral',
+        borderRadius: 4,
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
