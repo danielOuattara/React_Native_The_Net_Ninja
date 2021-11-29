7a6ee523a33426fe4e774e0bc7f1067a6669bb96
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-29 07:58:59 +0100

    todo part 3

---
+++ 11_Todo_App_Part_3/App.js
@@ -0,0 +1,80 @@
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import  Header from './components/Header'
+import TodoItem from './components/TodoItem';
+import AddTodoItem from './components/AddTodoItem';
+
+export default function App() {
+
+  const [ todos, setTodos ] = useState([
+    {text: 'buy coffee', completed: false, key: '1'},
+    {text: 'create an app', completed: false, completed: false, key: '2'},
+    {text: 'paly football', completed: false, key: '3'},
+  ]);
+
+  const handleRemoveItem = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(person => person.key !== key);
+    });
+  }
+
+  const handleAddTodoItem = (newTodoItem) => {
+    setTodos((previousTodos) => {
+      return (  [
+          ...previousTodos,
+          {
+            text: newTodoItem,
+            completed: false,
+            key: String(previousTodos.length + 1)
+          },
+      ])
+    });
+  }
+
+
+  return (
+    <View style={styles.container}> 
+       < Header />
+      <View style={styles.content}>
+        <AddTodoItem  handleAddTodoItem={handleAddTodoItem}/>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem 
+                item={item}
+                handleRemoveItem={handleRemoveItem}
+              />
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // body
+    flex: 1,
+    backgroundColor: '#fff',
+    borderColor: 'yellow',
+    borderStyle: 'solid',
+    borderWidth: 2
+  },
+
+  content: {
+    // main
+    padding: 40,
+    borderColor: 'green',
+    borderStyle:'solid',
+    borderWidth: 2,
+    marginBottom: 3,
+    marginLeft: 3,
+    marginRight: 3,
+  },
+  list: {
+    marginTop: 20,
+  },
+});
---
+++ 11_Todo_App_Part_3/components/AddTodoItem.js
@@ -0,0 +1,45 @@
+
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+
+
+export default function AddTodoItem({ handleAddTodoItem }) {
+
+    const [ newItemTitle, setNewItemTitle] = useState("");
+
+    return (
+        <View>
+            <TextInput 
+                style={styles.todoInput}
+                multiline
+                placeholder="Enter a new todo ..."
+                onChangeText= {(value) => setNewItemTitle(value)}
+            />
+            <TouchableOpacity onPress={() => handleAddTodoItem(newItemTitle)}>
+                <Text style={styles.todoItem}> ADD TODO</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    todoItem: {
+        padding: 10,
+        color: '#fff',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderRadius: 4,
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        backgroundColor: 'coral'
+    },
+
+    todoInput: {
+        borderBottomWidth: 1,
+        borderColor: "#777",
+        padding: 8,
+        marginBottom: 10,
+        fontSize: 20,
+    }
+});
