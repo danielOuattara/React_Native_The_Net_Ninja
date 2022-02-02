2e4162cfe5d8a9340f639e9f963d8d62c1409a1c
daniel ouattara <daniel.ouattara@gmx.com>
2022-02-02 16:52:05 +0100

    Passing data forward direction: OK

--- 21_Navigating_Around/screens/Home.js
+++ 21_Navigating_Around/screens/Home.js
@@ -1,24 +1,24 @@
 
 import React from 'react';
 import { Image, View, Text, ScrollView, Button } from 'react-native';
-import globalStyles from '../styles/global';
+import globalStyles from './../styles/global';
 
 
-export default function Home({navigation}) {
+export default function Home({ navigation }) {
     return (
         <View>
             <Button 
                 title='Go to reviews details'
                 onPress={ () => navigation.navigate('ReviewsDetails')}
             />
-         <ScrollView>
-            <View style={globalStyles.container}>
-                <Text style={globalStyles.titleText}>Welcome to Home Screen</Text>
-            </View>
-            <Image source={require('./../assets/flower.jpg')} /> 
-            <Image source={require('./../assets/flower2.jpg')} /> 
-            <Image source={require('./../assets/flower3.jpg')} /> 
-          </ScrollView>
+            <ScrollView>
+                <View style={globalStyles.container}>
+                    <Text style={globalStyles.titleText}>Welcome to Home Screen</Text>
+                </View>
+                <Image source={require('./../assets/flower.jpg')} /> 
+                <Image source={require('./../assets/flower2.jpg')} /> 
+                <Image source={require('./../assets/flower3.jpg')} /> 
+            </ScrollView>
         </View> 
     );
 }
--- 21_Navigating_Around/screens/ReviewDetails.js
+++ 21_Navigating_Around/screens/ReviewDetails.js
@@ -4,12 +4,21 @@ import { Image, View, Text, ScrollView, Button } from 'react-native';
 import globalStyles from '../styles/global';
 
 
-export default function ReviewDetails({navigation}) {
+export default function ReviewDetails({ navigation }) {
     return (
+        <View>
+            <View>
+                <Button 
+                    title='Go to Home'
+                    onPress={ () => navigation.navigate('Home')}
+                />
+            </View>
 
-                <View style={globalStyles.container}>
-                    <Text style={globalStyles.titleText}>Reviews DetailsScreen</Text>
-                </View>
+            <View style={globalStyles.container}>
+                <Text style={globalStyles.titleText}>Reviews DetailsScreen</Text>
+            </View>
+        </View>
+
 
     );
 }
