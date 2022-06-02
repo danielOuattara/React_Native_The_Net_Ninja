258e0cb225dd9488748a053db9bd47741119100a
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-02 23:28:58 +0200

    code review + formatting

--- 11_Todo_App_Part_3/components/TodoItem.js
+++ 11_Todo_App_Part_3/components/TodoItem.js
@@ -1,27 +1,24 @@
-
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
-
-
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export default function TodoItem({ item, handleRemoveItem }) {
-    return (
-        <TouchableOpacity onPress={() => handleRemoveItem(item.key)}>
-            <Text style={styles.todoItem}>{item.text}</Text>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity onPress={() => handleRemoveItem(item.key)}>
+      <Text style={styles.todoItem}>{item.text}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    todoItem: {
-        padding: 12,
-        marginTop: 16,
-        color: '#606060',
-        borderWidth: 1,
-        borderStyle: 'dashed',
-        borderColor: 'coral',
-        borderRadius: 8,
-        fontSize: 18,
-        textTransform: 'capitalize'
-    },
+  todoItem: {
+    padding: 12,
+    marginTop: 16,
+    color: "#606060",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "coral",
+    borderRadius: 8,
+    fontSize: 18,
+    textTransform: "capitalize",
+  },
 });
